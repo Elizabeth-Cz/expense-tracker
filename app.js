6d66a9444d37e6597dcfdb3b6805db81daee87d5
Elizabeth-Cz <87705292+Elizabeth-Cz@users.noreply.github.com>
2022-05-10 14:34:36 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Declairing DOM variables
+const account = [];
 const balance = document.querySelector("#balance");
 const minusBtn = document.querySelector("#minus-btn");
 const plusBtn = document.querySelector("#plus-btn");
@@ -6,123 +6,97 @@ const inputDes = document.querySelector("#input-description");
 const inputAmount = document.querySelector("#input-amount");
 const entriesDiv = document.querySelector(".entries");
 
-const account = {
-  incomes: [],
-  expenses: [],
+// ADD ENTRY DATE
+// const d = new Date();
+// const month = d.getMonth() + 1;
+// const day = d.getDate();
+// console.log(month);
+// console.log(day);
+// const timeStamp = `${day}.${month}`;
+// console.log(timeStamp);
 
-  addExpense(description, amount) {
-    this.expenses.push({
-      description: inputDes.value,
-      amount: parseInt(inputAmount.value) || 0,
-    });
-  },
-
-  addIncome(description, amount) {
-    this.incomes.push({
-      description: inputDes.value,
-      amount: parseInt(inputAmount.value) || 0,
-    });
-  },
-
-  getAccountSummery() {
-    let totalExpenses = 0;
-    let totalIncomes = 0;
-    let accountBalance = 0;
-    this.expenses.forEach((expense) => {
-      totalExpenses = totalExpenses + expense.amount;
-    });
-    this.incomes.forEach((income) => {
-      totalIncomes = totalIncomes + income.amount;
-    });
-    accountBalance = totalIncomes - totalExpenses;
-    return accountBalance;
-  },
-
-  reset() {
-    inputDes.value = "";
-    inputAmount.value = "";
-  },
-};
-
-// Render
-
-const renderIncomeEntry = function () {
-  //create divs
-  const entryDiv = document.createElement("div");
-  entryDiv.setAttribute("class", "entry");
-  entriesDiv.appendChild(entryDiv);
-
-  //create delete icon
-  const deleteIcon = document.createElement("i");
-  deleteIcon.setAttribute("class", "fa-solid fa-circle-xmark delete-btn");
-  entryDiv.appendChild(deleteIcon);
-  
-  // delete icon function **NOT REMOVING ENTRY FROM ARRAY YET**
-  deleteIcon.addEventListener("click", () => {
-    deleteIcon.parentNode.remove();
-  });
-  //create entry_title
-  const entryTitle = document.createElement("h4");
-  entryTitle.setAttribute("id", "entry_title");
-  entryDiv.appendChild(entryTitle);
-  entryTitle.textContent = inputDes.value;
-
-  //create entry_amount
-  const entryAmount = document.createElement("h4");
-  entryAmount.setAttribute("id", "entry_amount");
-  entryAmount.classList.add("income");
-  entryDiv.appendChild(entryAmount);
-  entryAmount.textContent = inputAmount.value;
-};
-
-const renderExpenseEntry = function () {
-  //create divs
-  const entryDiv = document.createElement("div");
-  entryDiv.setAttribute("class", "entry");
-  entriesDiv.appendChild(entryDiv);
-
-  //create delete icon
-  const deleteIcon = document.createElement("i");
-  deleteIcon.setAttribute("class", "fa-solid fa-circle-xmark delete-btn");
-  entryDiv.appendChild(deleteIcon);
-
-  // delete icon function
-  deleteIcon.addEventListener("click", () => {
-    deleteIcon.parentNode.remove();
-  });
-
-  //create entry_title
-  const entryTitle = document.createElement("h4");
-  entryTitle.setAttribute("id", "entry_title");
-  entryDiv.appendChild(entryTitle);
-  entryTitle.textContent = inputDes.value;
-
-  //create entry_amount
-  const entryAmount = document.createElement("h4");
-  entryAmount.setAttribute("id", "entry_amount");
-  entryAmount.classList.add("expense");
-  entryDiv.appendChild(entryAmount);
-  entryAmount.textContent = inputAmount.value;
-};
-
-// Event listeners
-
-minusBtn.addEventListener("click", function () {
-  if (inputAmount.value > 0 && inputDes.value) {
-    account.addExpense();
-    renderExpenseEntry();
-    account.reset();
-    console.log(account.getAccountSummery());
-    balance.innerHTML = account.getAccountSummery();
+const sumAccount = function () {
+  if (account.length === 0) {
+    balance.innerHTML = "0";
   }
+  let accountBalance = 0;
+  account.forEach((item) => {
+    accountBalance = accountBalance + item.amount;
+    // console.log(accountBalance);
+    if (accountBalance >= 0) {
+      balance.style.color = "#386641";
+    } else if (accountBalance < 0) {
+      balance.style.color = "#86090b";
+    }
+    balance.innerHTML = accountBalance;
+  });
+};
+
+const renderEntry = function (v) {
+  //create entry div
+  const entryDiv = document.createElement("div");
+  entryDiv.setAttribute("class", "entry");
+  entriesDiv.appendChild(entryDiv);
+
+  //create delete icon
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fa-solid fa-circle-xmark x");
+
+  //create entry title
+  const entryTitle = document.createElement("h4");
+  entryTitle.setAttribute("id", "entry_title");
+  entryTitle.textContent = inputDes.value;
+
+  //create entry amount
+  const entryAmount = document.createElement("h4");
+  entryAmount.setAttribute("id", "entry_amount");
+  entryAmount.textContent = inputAmount.value;
+  if (v) {
+    entryAmount.setAttribute("class", "income");
+  } else {
+    entryAmount.setAttribute("class", "expense");
+  }
+  entryDiv.append(deleteIcon, entryTitle, entryAmount);
+};
+
+plusBtn.addEventListener("click", () => {
+  if (inputAmount.value) {
+    account.push({
+      description: inputDes.value,
+      amount: parseInt(inputAmount.value),
+    });
+    sumAccount();
+    renderEntry(1);
+  }
+  inputDes.value = "";
+  inputAmount.value = "";
+  console.log(account);
 });
 
-plusBtn.addEventListener("click", function () {
-  if (inputAmount.value > 0 && inputDes.value) {
-    account.addIncome();
-    renderIncomeEntry();
-    account.reset();
-    console.log(account.getAccountSummery());
-    balance.innerHTML = account.getAccountSummery();
+minusBtn.addEventListener("click", () => {
+  if (inputAmount.value) {
+    account.push({
+      description: inputDes.value,
+      amount: parseInt(-inputAmount.value),
+    });
+    sumAccount();
+    renderEntry(0);
+  }
+  inputDes.value = "";
+  inputAmount.value = "";
+  console.log(account);
+});
+
+const delBtn = document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("x")) {
+    const iconList = Array.from(document.querySelectorAll(".x"));
+    iconIndex = iconList.indexOf(e.target);
+
+    // the amount to deduct from sum
+    console.log(account[iconIndex].amount);
+    account.splice(iconIndex, 1);
+    console.log(iconIndex);
+    e.target.parentElement.remove();
+    sumAccount();
   }
 });
