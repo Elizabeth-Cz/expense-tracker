d2be8c780e1c6cbe7b01d1cb46d322411c1abc2e
Elizabeth-Cz <87705292+Elizabeth-Cz@users.noreply.github.com>
2022-05-19 12:43:40 +0200

    update style and layout

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 let account = [];
 let incomes = [];
-let expenses = [];
+// let expenses = [];
+const expenses = [
+  // { title: "rent", amount: 950 },
+  // { title: "car wash", amount: 30 },
+  // {
+  //   title: "nails",
+  //   amount: 20,
+  // },
+];
+// const expensesTitles = expenses.map((x) => x.title);
+// const expensesAmounts = expenses.map((x) => x.amount);
+// const expensesData = [expensesTitles, expensesAmounts];
+// console.log(expensesData);
 let sumIncomes = 0;
 let sumExpenses = 0;
+// console.log(Object.values(expenses));
 const yValues = [sumIncomes, sumExpenses];
 const barColors = ["#adc698", "#c05746"];
 // const xValues = ["Incomes", "Expenses"];
 
 const balance = document.querySelector("#balance");
-const balanceContainer = document.querySelector(".container_balance");
+// const balanceContainer = document.querySelector(".container_balance");
 const minusBtn = document.querySelector("#minus-btn");
 const plusBtn = document.querySelector("#plus-btn");
-const inputDes = document.querySelector("#input-description");
-const inputAmount = document.querySelector("#input-amount");
+const titleInput = document.getElementById("input-description");
+const amountInput = document.getElementById("input-amount");
 const entriesDiv = document.querySelector(".entries");
+const incomesEl = document.getElementById("money-plus");
+const expensesEl = document.getElementById("money-minus");
 
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
@@ -71,102 +86,105 @@ const sumAccount = function () {
     sumIncomes = 0;
   }
   if (expenses.length > 0) {
-    sumExpenses = expenses.reduce((a, b) => a + b);
+    sumExpenses = expenses.reduce((sum, current) => {
+      return sum + current.amount;
+    }, 0);
   } else if (expenses.length === 0) {
     sumExpenses = 0;
   }
   let accountBalance = sumIncomes - sumExpenses;
-  // if (account.length > 0) {
-  //   accountBalance = account.reduce((a, b) => a + b);
-  // } else if (account.length === 0) {
-  //   balance.innerHTML = "0";
-  // }
+
   balance.innerHTML = accountBalance;
   // applying styling according to balance
   if (accountBalance > 0) {
-    balanceContainer.style.backgroundColor = "#adc698";
+    balance.style.backgroundColor = "#adc698";
     balance.style.color = "#f6f4f4";
   } else if (accountBalance < 0) {
-    balanceContainer.style.backgroundColor = "#c05746";
+    balance.style.backgroundColor = "#c05746";
     balance.style.color = "#f6f4f4";
   } else {
-    balanceContainer.style.backgroundColor = "#f6f4f4";
+    balance.style.backgroundColor = "#f6f4f4";
     balance.style.color = "gray";
     balance.innerHTML = "0";
   }
   console.log(`incomes:${sumIncomes}`);
   console.log(`expenses: ${sumExpenses}`);
-  console.log(`account balance: ${accountBalance}`);
+  console.log(accountBalance);
+  incomesEl.innerHTML = sumIncomes;
+  expensesEl.innerHTML = sumExpenses;
+  drawChart();
 };
 
 const renderEntry = function (v) {
   //create entry div
   const entryDiv = document.createElement("div");
-  entryDiv.setAttribute("class", "entry");
   entriesDiv.appendChild(entryDiv);
 
   //create delete icon
   const deleteIcon = document.createElement("i");
 
   //create entry title
-  const entryTitle = document.createElement("h4");
-  entryTitle.setAttribute("id", "entry_title");
-  entryTitle.textContent = inputDes.value;
+  const entryTitle = document.createElement("p");
+  entryTitle.setAttribute("class", "entry_title");
+  entryTitle.textContent = titleInput.value;
 
   //create info div with amount and date
   const entryInfo = document.createElement("div");
 
   //create entry amount
-  const entryAmount = document.createElement("h4");
-  entryAmount.setAttribute("id", "entry_amount");
-  entryAmount.textContent = inputAmount.value;
+  const entryAmount = document.createElement("p");
+  entryAmount.textContent = amountInput.value;
   if (v) {
-    entryAmount.setAttribute("class", "income");
+    entryDiv.setAttribute("class", "entry entry--income");
+    entryAmount.setAttribute("class", "income entry_amount");
     deleteIcon.setAttribute(
       "class",
-      "fa-solid fa-circle-xmark x income-delete"
+      "fa-solid fa-circle-xmark income-delete btn-delete"
     );
   } else {
-    entryAmount.setAttribute("class", "expense");
+    entryDiv.setAttribute("class", "entry entry--expense");
+    entryAmount.setAttribute("class", "expense entry_amount");
     deleteIcon.setAttribute(
       "class",
-      "fa-solid fa-circle-xmark x expense-delete"
+      "fa-solid fa-circle-xmark expense-delete btn-delete"
     );
   }
-
-  const entryDate = document.createElement("h3");
+  const entryDate = document.createElement("p");
   entryDate.setAttribute("class", "entry_date");
   entryDate.textContent = getTime();
 
   entryInfo.append(entryAmount, entryDate);
-  entryDiv.append(deleteIcon, entryTitle, entryInfo);
+  entryDiv.append(entryTitle, entryInfo, deleteIcon);
+  titleInput.value = "";
+  amountInput.value = "";
 };
 
 plusBtn.addEventListener("click", () => {
-  if (inputAmount.value) {
-    incomes.push(parseInt(inputAmount.value));
+  if (amountInput.value) {
+    incomes.push(parseInt(amountInput.value));
     renderEntry(1);
     sumAccount();
+    console.log(incomes);
   }
-  inputDes.value = "";
-  inputAmount.value = "";
-
-  console.log(incomes);
-  drawChart();
 });
 
 minusBtn.addEventListener("click", () => {
-  if (inputAmount.value) {
-    expenses.push(parseInt(inputAmount.value));
-    // expenses += parseInt(inputAmount.value);
+  if (amountInput.value) {
+    // let isTitleEqual = (element) => titleInput.value === element.title;
+    // const i = expenses.findIndex(isTitleEqual);
+    // console.log(i);
+    // if (i >= 0) {
+    //   expenses[i].amount += parseInt(amountInput.value);
+    // }
+    expenses.push({
+      title: titleInput.value,
+      amount: parseInt(amountInput.value),
+    });
+
     renderEntry(0);
     sumAccount();
   }
-  inputDes.value = "";
-  inputAmount.value = "";
-
   console.log(expenses);
-  drawChart();
 });
 
 const delBtn = document.addEventListener("click", function (e) {
@@ -174,10 +192,8 @@ const delBtn = document.addEventListener("click", function (e) {
     const iconList = Array.from(document.querySelectorAll(".income-delete"));
     iconIndex = iconList.indexOf(e.target);
     incomes.splice(iconIndex, 1);
-    console.log(iconIndex);
     e.target.parentElement.remove();
     sumAccount();
-    drawChart();
   } else if (e.target.classList.contains("expense-delete")) {
     const iconList = Array.from(document.querySelectorAll(".expense-delete"));
     iconIndex = iconList.indexOf(e.target);
@@ -185,7 +201,6 @@ const delBtn = document.addEventListener("click", function (e) {
     console.log(iconIndex);
     e.target.parentElement.remove();
     sumAccount();
-    drawChart();
   }
 });
 
@@ -201,9 +216,10 @@ coll.addEventListener("click", function () {
   }
 });
 
+// drawChart();
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ["transaction", "amount"],
+    ["expense", "amount"],
     ["incomes", sumIncomes],
     ["expenses", sumExpenses],
   ]);
